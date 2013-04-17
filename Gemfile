d862c3321aa6ded55ace2be026dfd75dbedb98b3
Lee Nelson <contact@leenelsondesign.com>
2013-04-16 22:21:39 -0700

    initial

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass-rails'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.6'
+gem 'devise'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicCollabz::Application.config.secret_token = 'e2dfc90bc37aa9d7024e9d42964346f8055d207badc3793d6ceaa61b5cab1d7c06bfd4145e3548eeac414840b5f3768b89ff5ddecbe516509e874eba518b01e0'
