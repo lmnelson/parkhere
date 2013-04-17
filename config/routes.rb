9e320708e48f86147aeb3766efc935f3a423091a
Lee Nelson <contact@leenelsondesign.com>
2013-04-17 11:24:25 -0700

    add twitter auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MusicCollabz::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users,  path_names: {sign_in: "login", sign_out: "logout"},
+                      controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   root to: 'static_pages#home'
   
